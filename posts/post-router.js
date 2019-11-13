7d82898012d0d471aff96f1eb558c9c1971e1c20
Kyle Barnett <kbarnett23@gmail.com>
2019-11-12 21:14:10 -0700

    work being done

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,15 +6,32 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  db('posts')
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving posts.' })
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+  const { id } = req.params;
+  db('posts').where({ id })
+    .then(post => {
+      if (post[0]) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({ message: 'ID not found.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving specific ID.' })
+    })
 });
 
 router.post('/', (req, res) => {
-
+  const postData = req.body;
 });
 
 router.put('/:id', (req, res) => {
