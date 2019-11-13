dd15521e4c0cf5b2286e32fcc8a7b2eae946c7a6
Kyle Barnett <kbarnett23@gmail.com>
2019-11-12 21:22:18 -0700

    guided project done

--- posts/post-router.js
+++ posts/post-router.js
@@ -32,14 +32,45 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const postData = req.body;
+
+  db('posts').insert(postData)
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error posting info.' })
+    })
 });
 
 router.put('/:id', (req, res) => {
-
+  const { id } = req.params;
+  const postInfo = req.body;
+  db('posts').where({ id }).update(postInfo)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({ message: 'ID not found.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error updating post.' })
+    })
 });
 
 router.delete('/:id', (req, res) => {
-
+  const { id } = req.params;
+  db('posts').where({ id }).del()
+    .then(post => {
+      if (post) {
+        res.status(204).json(post)
+      } else {
+        res.status(404).json({ message: 'ID not found.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error deleting post.' })
+    })
 });
 
 module.exports = router;
